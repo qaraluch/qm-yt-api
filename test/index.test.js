b0ce5c12bf05337df6a018dc18e2fb79207251cf
qaraluch <prezenciq@gmail.com>
2017-10-21 19:38:03 +0200

    Add api credentials for tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,28 @@
 import test from "ava";
-import XXX from "../dist/index.js";
+import ytAPI from "../dist/index.js";
+import credentials from "../.key.json";
 
-test("constructor function check", t => {
-  const msg = "should be a function i.e. construction fn.";
-  const actual = typeof XXX === "function";
-  const expected = true;
-  t.is(actual, expected, msg);
-});
-
-test("init", t => {
-  const msg1 = "should construction fn. return an object i.e. state";
-  const actual1 = typeof XXX() === "object";
+test("check credentials", t => {
+  const msg1 = "should load api key";
+  const actual1 = credentials.key.length > 0;
   const expected1 = true;
   t.is(actual1, expected1, msg1);
+  const msg2 = "should load playlist id";
+  const actual2 = credentials.playlist.length > 0;
+  const expected2 = true;
+  t.is(actual2, expected2, msg2);
 });
+
+// test("constructor function check", t => {
+//   const msg = "should be a function i.e. construction fn.";
+//   const actual = typeof ytAPI === "function";
+//   const expected = true;
+//   t.is(actual, expected, msg);
+// });
+
+// test("init", t => {
+//   const msg1 = "should construction fn. return an object i.e. state";
+//   const actual1 = typeof ytAPI() === "object";
+//   const expected1 = true;
+//   t.is(actual1, expected1, msg1);
+// });
