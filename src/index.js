b98ef2ce46c3cbd947e825b650e0de01adc12117
qaraluch <prezenciq@gmail.com>
2017-10-24 13:10:40 +0200

    Add feature of getting video info from YT playlist

--- src/index.js
+++ src/index.js
@@ -1,7 +1,120 @@
-import googleApis from "googleapis";
+import google from "googleapis";
 
-function XXX() {
-  return {};
-}
+const returnYTapi = apiKey =>
+  google.youtube({
+    version: "v3",
+    auth: apiKey
+  });
 
-export default XXX;
+const returnApiCB = (reject, resolve) => {
+  return (err, data, response) => {
+    var errData;
+    var reqData;
+    var resData;
+    if (err) {
+      errData = err;
+    } else if (data) {
+      reqData = data;
+    } else if (response) {
+      resData = "Status code: " + response.statusCode;
+    }
+    resolve([errData, reqData, resData]);
+  };
+};
+
+const asyncGetVideosInfo = (apiKey, endApiOptions) => {
+  return new Promise((resolve, reject) => {
+    const ytAPI = returnYTapi(apiKey);
+    // API info: https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.playlistItems.list
+    return ytAPI.playlistItems.list(
+      endApiOptions,
+      returnApiCB(reject, resolve)
+    );
+  });
+};
+
+const asyncGetPlaylistInfo = (apiKey, endApiOptions) => {
+  return new Promise((resolve, reject) => {
+    const ytAPI = returnYTapi(apiKey);
+    // API info: https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.playlists.list
+    return ytAPI.playlists.list(endApiOptions, returnApiCB(reject, resolve));
+  });
+};
+
+const margeOptions = (defaultOptions, passedOptions) =>
+  Object.assign({}, defaultOptions, passedOptions);
+
+const getDefaultOptionsForVideosInfo = playlistId => ({
+  part: "snippet, contentDetails",
+  playlistId
+});
+
+const getDefaultOptionsForPlayListInfo = playlistId => ({
+  part: "snippet, contentDetails",
+  id: playlistId
+});
+
+const mainErr = err => {
+  throw new Error(`qm-yt-api - getVideosInfoFromPlaylist(): ${err.message}`);
+};
+
+// Data structure:
+// playListInfo [errData, reqData, resData]
+// videosInfo [errData, reqData, resData]
+const processYTdata = (playlistId, playListInfo, videosInfo) => {
+  // let errInfos; //TODO: implement err mgmt
+  const pullVidiosInfo = obj => ({
+    position: obj.snippet.position,
+    id: obj.contentDetails.videoId,
+    title: obj.snippet.title
+  });
+  const dataProcessed = {
+    playlisName: playListInfo[1] && playListInfo[1].items[0].snippet.title,
+    playlistId,
+    itemsNumber:
+      playListInfo[1] && playListInfo[1].items[0].contentDetails.itemCount,
+    videos: videosInfo[1] && videosInfo[1].items.map(pullVidiosInfo)
+  };
+  return dataProcessed;
+};
+
+const getVideosInfoFromPlaylist = async (
+  apiKey,
+  playlistId,
+  apiOptions = {}
+) => {
+  const endApiOptionsForVideosInfo = margeOptions(
+    getDefaultOptionsForVideosInfo(playlistId),
+    apiOptions
+  );
+  const endApiOptionsForPlayListInfo = margeOptions(
+    getDefaultOptionsForPlayListInfo(playlistId),
+    apiOptions
+  );
+  try {
+    // get info from yt api
+    // TODO: concurrent for better performance ?
+    const playListInfo = await asyncGetPlaylistInfo(
+      apiKey,
+      endApiOptionsForPlayListInfo
+    );
+    const videosInfo = await asyncGetVideosInfo(
+      apiKey,
+      endApiOptionsForVideosInfo
+    );
+
+    // return [playListInfo, videosInfo]; //TODO: raw data from YT API - add flag
+    // console.log("videosInfo[1] ", videosInfo);
+
+    // process info
+    return processYTdata(playlistId, playListInfo, videosInfo);
+  } catch (error) {
+    mainErr(error);
+  }
+};
+
+const ytAPI = {
+  getVideosInfoFromPlaylist
+};
+
+export default ytAPI;
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,12 @@
 import test from "ava";
 import ytAPI from "../dist/index.js";
-import credentials from "../.key.json";
+import credentials from "../test/.key.json";
+import touch from "qm-fs-touch";
+
+// Tests are made against real YT data!
+const key = credentials.key;
+const playlist = credentials.playlist;
+const video = credentials.video;
 
 test("check credentials", t => {
   const msg1 = "should load api key";
@@ -11,18 +17,48 @@ test("check credentials", t => {
   const actual2 = credentials.playlist.length > 0;
   const expected2 = true;
   t.is(actual2, expected2, msg2);
+  const msg3 = "should load vidoe id";
+  const actual3 = credentials.video.length > 0;
+  const expected3 = true;
+  t.is(actual3, expected3, msg3);
 });
 
-// test("constructor function check", t => {
-//   const msg = "should be a function i.e. construction fn.";
-//   const actual = typeof ytAPI === "function";
-//   const expected = true;
-//   t.is(actual, expected, msg);
-// });
+test("getVideosInfoFromPlaylist() - constructor function check", t => {
+  const msg = "should be a function i.e. construction fn.";
+  const actual = typeof ytAPI.getVideosInfoFromPlaylist === "function";
+  const expected = true;
+  t.is(actual, expected, msg);
+});
 
-// test("init", t => {
-//   const msg1 = "should construction fn. return an object i.e. state";
-//   const actual1 = typeof ytAPI() === "object";
-//   const expected1 = true;
-//   t.is(actual1, expected1, msg1);
-// });
+test("getVideosInfoFromPlaylist() - return promise istance", t => {
+  const msg = "should return promise istance";
+  const actual = ytAPI.getVideosInfoFromPlaylist() instanceof Promise;
+  const expected = true;
+  t.is(actual, expected, msg);
+});
+
+const expectedInfoFromPlaylis = {
+  playlisName: "xxx-api-test",
+  playlistId: "PL14-o1976FEZbkTr9dDiDcDDUbMftdMjz",
+  itemsNumber: 2,
+  videos: [
+    {
+      position: 0,
+      id: "8CrOL-ydFMI",
+      title:
+        "This Is Water - Full version-David Foster Wallace Commencement Speech"
+    },
+    {
+      position: 1,
+      id: "RnGvWTRQ9j4",
+      title: "Everyday Virtue | Paterson & David Foster Wallace"
+    }
+  ]
+};
+
+test("getVideosInfoFromPlaylist() - get videos info from YT playlist", async t => {
+  const msg = "should get videos info from YT playlist";
+  const actual = await ytAPI.getVideosInfoFromPlaylist(key, playlist);
+  const expected = expectedInfoFromPlaylis;
+  t.deepEqual(actual, expected, msg);
+});
